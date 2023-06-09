b6b3c177f3ed0055da11403fda9b93dda075cbe2
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-06-09 16:02:11 +0800

    Good checkpoint

---
+++ online-portfolio/src/App.tsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./components/Navbar"
+import Home { Home } from "./pages/Home"
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+    </>
+  )
+}
+
+export default App
