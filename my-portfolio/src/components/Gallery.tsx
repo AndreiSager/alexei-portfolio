6b703850ee34a2d04d3ce2dfc667462b8841bfcf
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-06-12 17:04:27 +0800

    Item now properly displayed
    -Items are in reverse
    -Items are string interpolated
    -Items are images are properly referenced

--- my-portfolio/src/components/Gallery.tsx
+++ my-portfolio/src/components/Gallery.tsx
@@ -1,15 +1,15 @@
 import Projects from '../database/ProjectDatabase.json'
 
 export function Gallery() {
-    const projects = Projects;
+    const projects = Projects.reverse();
 
     return (
         <div className="flex justify-center text-center w-full">
-            <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 pb-12 max-w-screen-xl">
+            <div className="w-full grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 max-w-screen-xl">
                 {projects.map((project) => (
-                    <div className="flex flex-col min-w-full min-h-full aspect-square justify-center items-center">
-                        <img src={project.imgURl} alt={project.altMessage + " | " + project.itemType} className=''/>
-                        <h1>{project.title}</h1>
+                    <div key={project.id} className="flex flex-col min-w-full min-h-full aspect-square justify-center items-center">
+                        <img src={`src/assets/${project.imgURl}`} alt={`${project.itemType} | ${project.altMessage}`} className='min-w-full min-h-full aspect-square object-cover'/>
+                        <h1 className='hidden w-[80%]'>{project.title}</h1>
                     </div>
                 ))}
             </div>
