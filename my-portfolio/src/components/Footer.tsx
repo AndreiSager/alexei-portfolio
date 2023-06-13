ebb1111adc2381e23a1e408b2adfeee5f558262e
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-06-13 09:02:44 +0800

    Footer wrapped

--- my-portfolio/src/components/Footer.tsx
+++ my-portfolio/src/components/Footer.tsx
@@ -11,7 +11,7 @@ export function Footer() {
                     <a href="#" className='text-3xl font-bold text-neutral-2'>Back To Top</a>
                 </div>
                 <div className="flex flex-col-reverse md:flex-row justify-between p-8 gap-8">
-                    <h1 className='text-md flex justify-center items-center text-primary-1'>© 2023{nbsp}<a href="https://andrei-sager-portfolio.vercel.app" className='underline text-neutral-2'>Andrei Sager</a>{nbsp}& Alexei. All Rights Reserved.</h1>
+                    <h1 className='text-md flex flex-row flex-wrap justify-center items-center text-primary-1'>© 2023{nbsp}<a href="https://andrei-sager-portfolio.vercel.app" className='underline text-neutral-2'>Andrei Sager</a>{nbsp}& Alexei. All Rights Reserved.</h1>
                     <ul className='flex flex-row gap-4 justify-center text-primary-1'>
                         <li className='flex justify-center items-center hover:text-instagram hover:brightness-125'><a target="blank" href="https://instagram.com/creamy_poachegg?igshid=MzNlNGNkZWQ4Mg=="><BsInstagram size={iconSize}/></a></li>
                         <li className='flex justify-center items-center md:order-1 hover:text-facebook hover:brightness-125'><a target="blank" href="https://www.facebook.com/profile.php?id=100091338488777&mibextid=ZbWKwL"><BsFacebook size={iconSize}/></a></li>
