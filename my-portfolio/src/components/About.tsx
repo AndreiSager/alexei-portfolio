b322d70ad0d82910cb39fc0520afcf8ae72143b5
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-06-16 15:32:48 +0800

    Added an animated scroll down prompt

--- my-portfolio/src/components/About.tsx
+++ my-portfolio/src/components/About.tsx
@@ -1,14 +1,16 @@
 import image from '../../public/images/me.jpg'
+import { AiOutlineArrowDown } from 'react-icons/ai'
 
 export function About() {
     return (
-        <div className='flex justify-center'>
+        <div className='flex flex-wrap justify-center'>
             <div className="py-16 flex flex-wrap justify-center items-center gap-12 text-center max-w-screen-lg">
                 <img src={image} alt="" className="rounded-full aspect-square w-3/4 object-cover object-center shadow-sm drop-shadow-2xl md:w-2/5"/>
                 <div className='w-3/4'>
                     <h1 className='font-bold text-2xl md:text-3xl'>Who am I?</h1>
                     <p>Insert text here. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis, libero nisi! Autem excepturi perspiciatis a fugit voluptate neque quas dicta, dolor sed? Ratione, laborum repellendus obcaecati corporis doloribus vel maiores!</p>
                 </div>
+                <AiOutlineArrowDown size={35} className="w-full animate-bounce text-accent-1"/>
             </div>
         </div>
     )
