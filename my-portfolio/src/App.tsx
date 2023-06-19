10f95c62057f403ea8d015d1bf07b01e78db6de7
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-06-19 09:17:22 +0800

    Fuck skeletons in react

--- my-portfolio/src/App.tsx
+++ my-portfolio/src/App.tsx
@@ -1,16 +1,40 @@
+import { useState, useEffect } from 'react'
 import { About } from './components/About'
 import { Footer } from './components/Footer'
 import { Gallery } from './components/Gallery'
 import { Navbar } from './components/Navbar'
+import { ClockLoader } from 'react-spinners'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 3000);
+  }, [])
 
   return (
     <>
+    {isLoading ? 
+      <div className='flex h-[100vh] justify-center items-center'>
+            <ClockLoader
+            loading={isLoading}
+            size={150}
+            color='#7F5539'
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+      </div>
+    :
+    <div className={isLoading ? "hidden" : "block"}>
       <Navbar/>
       <About/>
       <Gallery/>
       <Footer/>
+    </div>
+    }
     </>
   )
 }
--- my-portfolio/src/components/Gallery.tsx
+++ my-portfolio/src/components/Gallery.tsx
@@ -21,7 +21,7 @@ export function Gallery() {
             <div className='flex flex-row max-w-screen-xl'>
                 <div className="w-full grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
                     {projects.map((project) => (
-                        <div key={project.id} className="relative min-w-full min-h-full aspect-square mx-auto ">
+                        <div key={project.id} className="relative min-w-full min-h-full aspect-square mx-auto">
                             <img src={`src/assets/${project.images.thumbnail}`} alt="No Thumbnail Image"
                                 className='relative aspect-square object-cover'/>
                             <div className={!modal ? 'absolute top-0 bottom-0 left-0 right-0 w-full h-full bg-opacity-0 text-opacity-0 duration-200 ease-linear bg-neutral-1 hover:bg-opacity-50 hover:text-opacity-100 text-primary-1 z-20 hover:z-0' : 'pointer-events-none hidden'}>
