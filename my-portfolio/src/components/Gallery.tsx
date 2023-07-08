ad4eb08e354061ee124a9ab87afdf3e6679c9b6f
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-08 22:13:15 +0800

    Fixed reverse bug
    +Apparently tailwind is incompatible to use grid-row-reverse

--- my-portfolio/src/components/Gallery.tsx
+++ my-portfolio/src/components/Gallery.tsx
@@ -1,31 +1,29 @@
 import { useState } from 'react';
 import { ImCross } from 'react-icons/im'
-import { GalleryItemData } from '../constants/index.ts'
+import { GalleryItemData as gallery} from '../constants/index.ts'
 
 export function Gallery() {
-    const gallery = GalleryItemData.slice().reverse();
-
-    const [modal, setModal] = useState(false);
+    const [modal, setModal] = useState<true | false>(false);
     const handleModal = () => {
         setModal(!modal)
     }
-    const [i, setIndex] = useState(0);
-    function updateContent(id: number): void {
-        setIndex(id - 1);
+    const [index, setIndex] = useState<number>(0);
+    function updateContent(index: number): void {
+        setIndex(index);
         handleModal();
     }
 
     return (
         <div className="flex justify-center text-center w-full">
             <div className='flex flex-row max-w-screen-xl'>
-                <div className="w-full grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
-                    {gallery.map((project) => (
-                        <div key={project.id} className="relative min-w-full min-h-full aspect-square mx-auto">
+                <div className="w-full grid grid-row-reverse grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+                    {gallery.map((project, index) => (
+                        <div className="relative min-w-full min-h-full aspect-square mx-auto">
                             <img 
-                                src={`https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/thumbnails/thumbnail-${project.id}.jpg`}
+                                src={`https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/${project.album}/${project.thumbnail}`}
                                 srcSet={`
-                                      https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-400/thumbnails/thumbnail-${project.id}.jpg?width=400 400w,
-                                      https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-800/thumbnails/thumbnail-${project.id}.jpg?width=800 800w
+                                      https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-400/${project.album}/${project.thumbnail}?width=400 400w,
+                                      https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-800/${project.album}/${project.thumbnail}?width=800 800w
                                 `}
                                 alt="No Thumbnail Image"
                                 loading='lazy'
@@ -34,9 +32,9 @@ export function Gallery() {
                             />
                             <div className={!modal ? 'absolute top-0 bottom-0 left-0 right-0 w-full h-full bg-opacity-0 text-opacity-0 duration-200 ease-linear bg-neutral-1 hover:bg-opacity-50 hover:text-opacity-100 text-primary-1 z-20 hover:z-0' : 'pointer-events-none hidden'}>
                                 <div className='relative w-[80%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                                    <button onClick={() => updateContent(project.id)} className='text-2xl md:text-3xl underline font-extrabold'>{project.content.title}</button>
-                                    <h1 className='mt-2 text-sm font-bold z-10'>{project.content.date}</h1>
-                                    <h1 className='text-sm z-10'>{project.content.itemType}</h1>
+                                    <button onClick={() => updateContent(index)} className='text-2xl md:text-3xl underline font-extrabold'>{project.title}</button>
+                                    <h1 className='mt-2 text-sm font-bold z-10'>{project.date}</h1>
+                                    <h1 className='text-sm z-10'>{project.itemType}</h1>
                                 </div>
                             </div>
                         </div>
@@ -46,30 +44,30 @@ export function Gallery() {
             <div className={modal ? 'fixed justify-center z-10 left-0 top-0 w-full h-full overflow-auto bg-[rgba(0,0,0,0.4)] pointer-events-auto backdrop-blur-sm' : "hidden"}>
                 <div className='bg-primary-1 mx-full md:max-w-screen-md md:mx-auto p-12 md:p-16 md:my-20 pointer-events-none z-20 flex flex-col gap-8'>
                     <div className='flex flex-row justify-between pointer-events-none'>
-                        <h1 className='font-medium text-xl'>{gallery[i].content.title}</h1>
+                        <h1 className='font-medium text-xl'>{gallery[index].title}</h1>
                         <span onClick={handleModal} className='pointer-events-auto hover:text-neutral-4'><ImCross id='exit' size={20}/></span>
                     </div>
                     <div className='flex flex-wrap flex-col md:flex-row text-left gap-6'>
                         <div className='flex flex-col text-md font-serif text-slate-600'>
-                            <h6>Role: {gallery[i].content.role}</h6>
-                            <h6>Client: {gallery[i].content.client}</h6>
-                            <h6>{gallery[i].content.url}</h6>
+                            <h6>Role: {gallery[index].role}</h6>
+                            <h6>Client: {gallery[index].client}</h6>
+                            <h6>{gallery[index].url}</h6>
                         </div>
                         <div>
-                            <p className='text-lg text-md w-full font-normal'>{gallery[i].content.description}</p>
+                            <p className='text-lg text-md w-full font-normal'>{gallery[index].description}</p>
                         </div>
                     </div>
                     <div className='flex flex-wrap gap-8'>
-                        {gallery[i].images.map((image) => (
-                            <div key={i} className='w-fit flex flex-col gap-2'>
+                        {gallery[index].images.map((image) => (
+                            <div className='w-full flex flex-col gap-2'>
                                 <img 
-                                    src={`https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/project-${i}/image-${image.id}.jpg?`}
+                                    src={`https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/${gallery[index].album}/${image.image}`}
                                     srcSet={`
-                                          https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-400/project-${i}/image-${image.id}.jpg?width=400 400w,
-                                          https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-800/project-${i}/image-${image.id}.jpg?width=800 800w,
-                                          https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-1200/project-${i}/image-${image.id}.jpg?width=1200 1200w
+                                            https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-400/${gallery[index].album}/${image.image}?width=400 400w,
+                                            https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-800/${gallery[index].album}/${image.image}?width=800 800w,
+                                            https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/tr:w-1200/${gallery[index].album}/${image.image}?width=1200 1200w
                                     `}
-                                    alt=""
+                                    alt={image.alt}
                                     loading='lazy'
                                     decoding="async"
                                 />
--- my-portfolio/src/constants/index.ts
+++ my-portfolio/src/constants/index.ts
@@ -1,5 +1,5 @@
 export const ReferenceLinks = [
-    { title: "Andrei Sager", url: "https://andrei-sager-portfolio.vercel.app"},
+    { title: "Andrei Sager", url: "https://andreisager.vercel.app"},
 ]
 
 export const SocialMediaLinks = [
@@ -12,84 +12,85 @@ export const AboutContent = {
     imageKitId: "https://ik.imagekit.io/alyujsrzv/Alexei-Portfolio/",
     imageAvatar: "compressed/me.jpg",                   
     title: "Who Am I?",
-    description: ", I am a highly motivated and enthusiastic individual with a strong passion for creating exceptional liquor and liqueurs. I am currently seeking a challenging role where I can fully utilize my potential, enhance my ethical and practical skills, and make a meaningful contribution to a company.",
+    description: "I am a highly motivated and enthusiastic individual with a strong passion for creating exceptional liquor and liqueurs. I am currently seeking a challenging role where I can fully utilize my potential, enhance my ethical and practical skills, and make a meaningful contribution to the company.",
 }
 
 export const GalleryItemData = [
     {
-        id : 1, 
-        content: { 
-            title: "SCREW DRIVER",
-            date: "2020",
-            itemType: "Reciepe",
-            description: "I ¤ 1/2 oz | 15ml | Lime ¤ 1/2 oz | 15ml | Lemon  ¤ 1: 1/2 oz  | 45ml | Orange  ¤ 1 oz or 3/4 oz | 15ml  | Agave syrup ▪︎ Tip:  2 Times /2 of orange & then 2 times 1:1/2 or 1 of orangehave created my own recipe",
-            client: "None",
-            role: "Bartender",
-            url: "",
-        },
+        thumbnail: "swiss-bail.jpg",
+        alt: "Swiss Bail",
+        title: "SCREW DRIVER",
+        date: "2020",
+        itemType: "Recipe",
+        description: "I ¤ 1/2 oz | 15ml | Lime ¤ 1/2 oz | 15ml | Lemon  ¤ 1: 1/2 oz  | 45ml | Orange  ¤ 1 oz or 3/4 oz | 15ml  | Agave syrup ▪︎ Tip:  2 Times /2 of orange & then 2 times 1:1/2 or 1 of orangehave created my own recipe",
+        client: "None",
+        role: "Bartender",
+        url: "",
+        album: "project-0",
         images: [
-            { id: 1, note: "You are gay", }
+            { image: "image-1.jpg", alt:"Swiss Bail Photo", caption:"A photo of the swiss bail using an old camera." },
+            { image: "image-1.jpg", alt:"Swiss Bail Photo", caption:"A photo of the swiss bail using an old camera." },
         ]
     },
-    {
-        id: 2, 
-        content: { 
-            title: "SWISS BAIL",
-            date: "2020",
-            itemType: "Recipe",
-            description: " sheeesh",
-            client: "None",
-            role: "Bartender",
-            url: "",
-        },
-        images: [
-            { id: 2, note: "You are gay", },
-        ],
-    },
-    {
-            id : 3, 
-            content: { 
-                title: "YELLOW JACKSON",
-                date: "2020",
-                itemType: "Recipe",
-                description: "I have created my own recipe",
-                client: "None",
-                role: "Bartender",
-                url: "",
-            },
-        images: [
-            { id: 3, note: "You are gay", }
+    // {
+    //     id: 2, 
+    //     content: { 
+    //         title: "SWISS BAIL",
+    //         date: "2020",
+    //         itemType: "Recipe",
+    //         description: " sheeesh",
+    //         client: "None",
+    //         role: "Bartender",
+    //         url: "",
+    //     },
+    //     images: [
+    //         { id: 1, note: "You are gay", },
+    //     ],
+    // },
+    // {
+    //         id : 3, 
+    //         content: { 
+    //             title: "YELLOW JACKSON",
+    //             date: "2020",
+    //             itemType: "Recipe",
+    //             description: "I have created my own recipe",
+    //             client: "None",
+    //             role: "Bartender",
+    //             url: "",
+    //         },
+    //     images: [
+    //         { id: 1, note: "You are gay", }
 
-        ]
-    },
-    {
-        id : 4, 
-        content: { 
-            title: "CUBA LIVER",
-            date: "2023",
-            itemType: "Recipe",
-            description: "¤ 3/4oz | 20ml | Lime ¤ 1/2 oz | 15ml | Lemon ¤ 1 oz or 3/4 oz | 15ml | Agavesyrup Optional: Lemon and use your favorite Coca-Cola soda.",
-            client: "None",
-            role: "Bartender",
-            url: "",
-        },
-        images: [
-            { id: 4, note: "You are gay", }
-        ]
-    },
-    {
-        id : 5, 
-        content: { 
-            title: "LEMON MARGARITA",
-            date: "2023",
-            itemType: "Recipe",
-            description: " ¤ 1:1/2 oz | 45ml | Lime ¤ 1/2 oz | 15ml | Lemon  ¤ 1 oz or 3/4 oz | 15ml Agavesyrup¤ Lemonade / Orange Soda  ▪︎ Tip: if you want to use Sanpelligrino if not its fine. ▪︎Optional: Ginger ALE",
-            client: "None",
-            role: "Bartender",
-            url: "",
-        },
-        images: [
-            { id: 5, note: "You are gay", }
-        ]
-    },
+    //     ]
+    // },
+    // {
+    //     id : 4, 
+    //     content: { 
+    //         title: "CUBA LIVER",
+    //         date: "2023",
+    //         itemType: "Recipe",
+    //         description: "¤ 3/4oz | 20ml | Lime ¤ 1/2 oz | 15ml | Lemon ¤ 1 oz or 3/4 oz | 15ml | Agavesyrup Optional: Lemon and use your favorite Coca-Cola soda.",
+    //         client: "None",
+    //         role: "Bartender",
+    //         url: "",
+    //     },
+    //     images: [
+    //         { id: 1, note: "You are gay", }
+    //     ]
+    // },
+    // {
+    //     id : 5, 
+    //     content: { 
+    //         title: "LEMON MARGARITA",
+    //         date: "2023",
+    //         itemType: "Recipe",
+    //         description: "Hello ¤ 1:1/2 oz | 45ml | Lime ¤ 1/2 oz | 15ml | Lemon  ¤ 1 oz or 3/4 oz | 15ml Agavesyrup¤ Lemonade / Orange Soda  ▪︎ Tip: if you want to use Sanpelligrino if not its fine. ▪︎Optional: Ginger ALE",
+    //         client: "None",
+    //         role: "Bartender",
+    //         url: "",
+    //     },
+    //     images: [
+    //         { id: 1, note: "You are gay", }
+    //     ]
+    // },
 ]        
