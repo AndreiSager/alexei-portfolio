47814802091f578ccaaf5488597023aa019d4435
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-24 06:37:25 +0800

    Update Navbar.tsx
    +Added a reusable component
    -Removed unnecessary code

--- my-portfolio/src/components/Navbar.tsx
+++ my-portfolio/src/components/Navbar.tsx
@@ -1,43 +1,12 @@
-import { SocialMediaLinks } from '../constants';
-import { BsInstagram, BsFacebook } from 'react-icons/bs'
-import { ImLinkedin } from 'react-icons/im'
-
+import SocialMediaLinks from "./SocialMedia";
 
 export function Navbar() {
-    const iconSize = 30;
-    const link = SocialMediaLinks;
-
     return (
         <div className="flex flex-wrap justify-center items-center text-center w-full">
             <div className="flex md:flex-row flex-wrap gap-y-8 justify-center md:justify-between md:gap-auto items-center p-8 w-full max-w-screen-xl">
-                {/* Hidden component to equalize justify */}
-                <ul className='hidden md:invisible md:flex md:flex-row gap-4 justify-center'>
-                    <li className='flex justify-center items-center'><a target="blank" href=""><BsInstagram size={iconSize}/></a></li>
-                    <li className='flex justify-center items-center'><a target="blank" href=""><BsFacebook size={iconSize}/></a></li>
-                    <li className='flex justify-center items-center'><a target="blank" href=""><ImLinkedin size={iconSize}/></a></li>
-                </ul>
-                {/* Hidden component to equalize justify */}
+                <SocialMediaLinks hidden/>
                 <h1 className='text-5xl font-black bg-clip-text wk-bg-clip text-transparent bg-cover bg-center bg-home-texture'>ALEXEI</h1>
-                <ul className='flex flex-row gap-4 justify-center text-neutral-3 w-full md:w-fit'>
-                    <li className='flex justify-center items-center hover:text-instagram'>
-                        <a target="_blank" 
-                            href={link[0].url}>
-                            <BsInstagram size={iconSize}/>
-                        </a>
-                    </li>
-                    <li className='flex justify-center items-center hover:text-facebook'>
-                        <a target="_blank" 
-                            href={link[1].url}>
-                            <BsFacebook size={iconSize}/>
-                        </a>
-                    </li>
-                    <li className='flex justify-center items-center hover:text-linkedin'>
-                        <a target="_blank" 
-                            href={link[2].url}>
-                            <ImLinkedin size={iconSize}/>
-                        </a>
-                    </li>
-                </ul>
+                <SocialMediaLinks/>
             </div>
         </div>
     )
