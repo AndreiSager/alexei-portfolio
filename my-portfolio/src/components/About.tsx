2ee95e3d20df735b6d9778200790b858c40d27e2
LordBrim <107153674+LordBrim@users.noreply.github.com>
2023-07-24 10:04:13 +0800

    Updated

--- my-portfolio/src/components/About.tsx
+++ my-portfolio/src/components/About.tsx
@@ -5,8 +5,8 @@ export function About() {
     const content = AboutContent;
 
     return (
-        <div className='flex flex-wrap justify-center'>
-            <div className="py-16 flex flex-wrap justify-center items-center gap-12 text-center max-w-screen-lg">
+        <div className='w-full flex flex-wrap justify-center'>
+            <div className="lg:py-8 flex flex-wrap justify-center items-center text-center max-w-screen-xl">
                 <img
                     src={content.imageAvatar}
                     srcSet={`
@@ -15,13 +15,13 @@ export function About() {
                         ${content.imageKitId}tr:w-1200/${content.imageAvatar}?width=1200 1200w,
                     `}
                     alt="A picture of myself with a mask, me Alexei"
-                    className="rounded-full aspect-square w-3/4 object-cover object-center shadow-sm drop-shadow-2xl md:w-2/5"
+                    className="rounded-full aspect-square w-3/4 lg:w-2/5 object-cover object-center shadow-sm drop-shadow-2xl md:w-2/5"
                 />
-                <div className='w-3/4'>
+                <div className='w-full p-8'>
                     <h1 className='font-bold text-2xl md:text-3xl'>{content.title}</h1>
-                    <p className='m-4'>{content.description}</p>
+                    <p className='mt-4 lg:mx-16'>{content.description}</p>
                 </div>
-                <AiOutlineArrowDown size={35} className="w-full animate-bounce text-accent-1"/>
+                <AiOutlineArrowDown size={35} className="w-full animate-bounce text-accent-1 mb-6"/>
             </div>
         </div>
     )
--- my-portfolio/src/components/Gallery.tsx
+++ my-portfolio/src/components/Gallery.tsx
@@ -16,7 +16,7 @@ export function Gallery() {
     return (
         <div className="flex justify-center text-center w-full">
             <div className='flex flex-row max-w-screen-xl'>
-                <div className="w-full grid grid-row-reverse grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
+                <div className="w-full grid grid-flow-row-dense grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                     {gallery.map((project) => (
                         <div key={project.id} className="relative min-w-full min-h-full aspect-square mx-auto">
                             <img 
